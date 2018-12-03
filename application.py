1f1cbbcf4a0a892d9f95f242ec40b8d0825944c1
Joerockafella <joeshindano@outlook.com>
2018-12-03 18:21:12 +0200

    index page updated with the header and the navbar

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -20,11 +20,11 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
+@app.route("/home")
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template('index.html', title='Home')
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run(debug=True)
