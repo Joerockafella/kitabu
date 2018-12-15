818711487bd78b4fbe3853cb480681d1ab4c1186
Joerockafella <joeshindano@outlook.com>
2018-12-15 15:52:15 +0200

    updating the database

--- application.py
+++ application.py
@@ -5,12 +5,15 @@ import json
 from flask import Flask, session, render_template, request, redirect, url_for, Markup, flash
 from flask_session import Session
 from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import scoped_session, sessionmaker
 from forms import RegistrationForm, LoginForm
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'SECRET_KEY'
+bcrypt = Bcrypt(app)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -35,8 +38,15 @@ def index():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Hey {form.username.data}, your account has been created.', 'success')
-        return redirect(url_for('index'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        username = form.username.data 
+        email = form.email.data
+        password = hashed_password
+        db.execute("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)",
+                {"username": username, "email": email, "password": password})
+        db.commit()
+        flash(f'Hey {form.username.data}, your account has been created! You are now able to login.', 'success')
+        return redirect(url_for('login'))
     return render_template("register.html", title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
--- import.py
+++ import.py
@@ -11,7 +11,7 @@ engine= create_engine(os.getenv("DATABASE_URL"))
 db =scoped_session(sessionmaker(bind=engine))
 
 def main():
-    db.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR NOT NULL, password VARCHAR NOT NULL)")
+    db.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR NOT NULL, email VARCHAR NOT NULL, password VARCHAR NOT NULL)")
     db.execute("CREATE TABLE reviews (isbn VARCHAR NOT NULL,review VARCHAR NOT NULL, rating INTEGER NOT NULL,username VARCHAR NOT NULL)")
     db.execute("CREATE TABLE books (isbn VARCHAR PRIMARY KEY,title VARCHAR NOT NULL,author VARCHAR NOT NULL,year VARCHAR NOT NULL)")
     f=open("books.csv")
