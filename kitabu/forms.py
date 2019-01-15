4be7e8096d65274e3d74d518c5595a0744c9f1e5
Joerockafella <joeshindano@outlook.com>
2019-01-15 10:51:30 +0200

    major update kitabu 1.0

--- kitabu/forms.py
+++ kitabu/forms.py
@@ -1,16 +1,8 @@
-import os
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from kitabu.models import User
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
-engine= create_engine(os.getenv("DATABASE_URL"))
-db =scoped_session(sessionmaker(bind=engine))
 
 #Creating the Registration Form
 class RegistrationForm(FlaskForm):
@@ -20,22 +12,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    # Validating the username, making sure it doesn't already exist
     def validate_username(self, username):
-        form = RegistrationForm()
-        username = form.username.data 
-        check_user = db.execute("SELECT * FROM users WHERE username = :username", {"username": username})
-        user = check_user.first()
+        user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
-    
+
     def validate_email(self, email):
-        form = RegistrationForm()
-        email = form.email.data 
-        check_user = db.execute("SELECT * FROM users WHERE email = :email", {"email": email})
-        user = check_user.first()
+        user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
-
 
 #Creating the Login Form
 class LoginForm(FlaskForm):
---
+++ kitabu/models.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from kitabu import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# Automatically creates a table wth the name 'user' small letter
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    user_reviews = db.relationship('Review', backref='author', lazy=True)
+
+    def __repr__(self):
+            return f"User('{self.username}', '{self.email}' )" #this is what will be printed
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    rating = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+
+    def __repr__(self):
+            return f"Review('{self.title}', '{self.date_posted}', '{self.rating}' )" 
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer)
+    book_reviews = db.relationship('Review', lazy=True)
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.date_posted}', '{self.rating}' )" 
