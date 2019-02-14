24925a6a33aef2e67c37dc3805da346f5369e8af
Joerockafella <joeshindano@outlook.com>
2019-02-14 13:43:25 +0200

    updates

--- run.py
+++ run.py
@@ -3,15 +3,15 @@ from kitabu import create_app, db
 
 app = create_app()
 app.app_context()
-app.app_context().push()
+#app.app_context().push()
 db.init_app(app)
-db.drop_all()
-db.create_all()
+#db.drop_all()
+#db.create_all()
 
 #This is for importing all the books data in db
-from all_books import main
+#from all_books import main
 
-main()
+#main()
 
 if __name__ == "__main__":
     app.run(debug=True)
